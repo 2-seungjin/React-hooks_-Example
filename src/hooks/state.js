895fb75d4a0192ad8055ae92b2ad33aa6fbfcfd6
leeseungjin <dltmdwls154@gmail.com>
2021-06-24 01:38:39 +0900

    state count 간단하게

--- src/hooks/state.js
+++ src/hooks/state.js
@@ -1,10 +1,14 @@
 import React,{useState} from 'react';
 
 const State = () => {
-  const [text, setText] = useState('');
+  const [value, setValue] = useState(0);
   return (
     <div>
-      <input type="text"/>
+      <p>
+        현제 카운터 값은  <b>{value}</b> 입니다
+      </p>
+      <button onClick={() => setValue(value +1)}>+1</button>
+      <button onClick={() => setValue(value -1)}>-1</button>
     </div>
   );
 }
