7df9c5b6b3cb1f83d738a699818bbf16efb4555e
LeeSeungJin <dltmdwls154@gmail.com>
2022-09-11 23:18:44 +0900

    antd node 모듈 에러 발생

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import React, { useState } from "react";
-import Average from './hooks/useMemo/useMemo'
-import Effect3 from './hooks/useEffect/Effect3'
+import React  from "react";
+import MainLayout from './layout/layout'
 
-//const App = () => {
-//   return (
-//     <div>
-//       <Average />
-//     </div>
-//   );
-// }
-//
-// export default App;
+const App = () => {
+   return (
+    < MainLayout />
+   );
+ }
+
+ export default App;
 
 
 // useEffect3 화면 가리기, 보이기 렌더링 직후 특정값 변경시 이벤트
-function App() {
-  const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button onClick={() => {setVisible(!visible)}}>
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr/>
-      {visible && <Effect3 />}
-    </div>
-  );
-}
-export default App
+//function App() {
+//   const [visible, setVisible] = useState(false);
+//   return (
+//     <div>
+//       <button onClick={() => {setVisible(!visible)}}>
+//         {visible ? '숨기기' : '보이기'}
+//       </button>
+//       <hr/>
+//       {visible && <Effect3 />}
+//     </div>
+//   );
+// }
+// export default App
---
+++ src/layout/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header } from 'antd/es/layout/layout'
+
+
+const MainHeader = () => {
+
+  return (
+    <Header className="header">
+      <div className="logo" />
+      asdasd
+    </Header>
+  );
+};
+
+export default MainHeader;
