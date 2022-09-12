ace51bfbc1722f0cee1fc1e34fde0a0acea50acf
Seungjin Lee <dltmdwls154@gmail.com>
2022-09-12 23:37:50 +0900

    inputs edit

--- src/hooks/useState/StateInputs.jsx
+++ src/hooks/useState/StateInputs.jsx
@@ -3,46 +3,60 @@ import CodeBlocks from '../../components/CodeBlock';
 import { Form, Input } from 'antd';
 
 const StateInputs = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
+  const [name, setName] = useState({
+    name: '',
+    nickName: '',
+  });
+  const onChange = ({ target }) => {
+    const { value, id } = target;
+    setName({
+      ...name,
+      [id]: value,
+    });
   };
 
   const codeBlockText =
     'const StateInputs = () => {\n' +
-    "  const [name, setName] = useState('');\n" +
-    "  const [nickname, setNickname] = useState('');\n" +
-    '\n' +
-    '  const onChangeName = (e) => {\n' +
-    '    setName(e.target.value);\n' +
+    '  const [name, setName] = useState({\n' +
+    "    name: '',\n" +
+    "    nickName: '',\n" +
+    '  });\n' +
+    '  const onChange = ({ target }) => {\n' +
+    '    const { value, id } = target;\n' +
+    '    setName({\n' +
+    '      ...name,\n' +
+    '      [id]: value,\n' +
+    '    });\n' +
     '  };\n' +
     '\n' +
-    '  const onChangeNickname = (e) => {\n' +
-    '    setNickname(e.target.value);\n' +
-    '  };\n' +
-    '\n' +
+    "  const codeBlockText = '';\n" +
     '  return (\n' +
     '    <div>\n' +
-    '      <div>\n' +
-    '        <span>\n' +
-    '          name : <input type="text" value={name} onChange={onChangeName} />\n' +
-    '        </span>\n' +
-    '        <span>\n' +
-    '          nickName : <input type="text" value={nickname} onChange={onChangeNickname} />\n' +
-    '        </span>\n' +
-    '      </div>\n' +
+    '      <Form\n' +
+    '        name="basic"\n' +
+    '        labelCol={{ span: 4 }}\n' +
+    '        wrapperCol={{ span: 10 }}\n' +
+    '        initialValues={{ remember: true }}\n' +
+    '        autoComplete="off">\n' +
+    '        <Form.Item\n' +
+    '          label="Name"\n' +
+    '          name="Name"\n' +
+    "          rules={[{ required: true, message: 'Please input your Name!' }]}>\n" +
+    "          <Input id={'name'} value={name.name} onChange={onChange} />\n" +
+    '        </Form.Item>\n' +
+    '        <Form.Item\n' +
+    '          label="Nick Name"\n' +
+    '          name="Nick Name"\n' +
+    "          rules={[{ required: true, message: 'Please input your Nick Name!' }]}>\n" +
+    "          <Input id={'nickName'} value={name.nickName} onChange={onChange} />\n" +
+    '        </Form.Item>\n' +
+    '      </Form>\n' +
     '      <div>\n' +
     '        <div>\n' +
-    '          <b>이름 : </b> {name}\n' +
+    '          <b>이름 : </b> {name.name}\n' +
     '        </div>\n' +
     '        <div>\n' +
-    '          <b>닉네임 : </b> {nickname}\n' +
+    '          <b>닉네임 : </b> {name.nickName}\n' +
     '        </div>\n' +
     '      </div>\n' +
     '    </div>\n' +
@@ -52,7 +66,7 @@ const StateInputs = () => {
     <div>
       <Form
         name="basic"
-        labelCol={{ span: 4 }}
+        labelCol={{ span: 10 }}
         wrapperCol={{ span: 10 }}
         initialValues={{ remember: true }}
         autoComplete="off">
@@ -60,21 +74,21 @@ const StateInputs = () => {
           label="Name"
           name="Name"
           rules={[{ required: true, message: 'Please input your Name!' }]}>
-          <Input value={name} onChange={onChangeName} />
+          <Input id={'name'} value={name.name} onChange={onChange} />
         </Form.Item>
         <Form.Item
           label="Nick Name"
           name="Nick Name"
           rules={[{ required: true, message: 'Please input your Nick Name!' }]}>
-          <Input value={nickname} onChange={onChangeNickname} />
+          <Input id={'nickName'} value={name.nickName} onChange={onChange} />
         </Form.Item>
       </Form>
       <div>
         <div>
-          <b>이름 : </b> {name}
+          <b>이름 : </b> {name.name}
         </div>
         <div>
-          <b>닉네임 : </b> {nickname}
+          <b>닉네임 : </b> {name.nickName}
         </div>
       </div>
 
